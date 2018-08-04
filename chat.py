d241c3c45c32dbaf0b55c5359ee9a39831e6708c
EricSzcz <e.ericszcz@gmail.com>
2018-08-04 20:10:51 -0300

    New Version

--- chat.py
+++ chat.py
@@ -1,49 +1,38 @@
-import socket
-import threading
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 import time
 
-tLock = threading.Lock()
-shutdown = False
-
-def receving(name, sock):
-    while not shutdown:
-        try:
-            tLock.acquire()
-            while True:
-                # data, addr = sock.recv(1024).decode('utf-8')
-                data, addr = sock.accept()
-                # data = sock.recv(1024).decode('utf-8')
-                print(data.recv(1024).decode('utf-8'))
-        except:
-            pass
-        finally:
-            tLock.release()
 
 host = '127.0.0.1'
 port = 5000
+bufsiz = 1024
+addr = (host, port)
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(addr)
 
-server = ('127.0.0.1',5000)
 
-s = socket.socket()
-print(host)
-print(port)
-s.connect((host, port))
-# s.setblocking(0)
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(bufsiz).decode("utf-8")
+            print(msg)
+        except OSError:
+            break
 
-rT = threading.Thread(target=receving, args=("RecvThread",s))
-rT.start()
 
-alias = input("Name: ")
-message = input(alias + "-> ")
-while message != 'q':
-    if message != '':
-        message = alias + ": " + message        
-        s.sendto(message.encode(), server)
-    tLock.acquire()
-    message = input(alias + "-> ")
-    tLock.release()
+def send():
     time.sleep(0.2)
+    msg = input()
+    if msg != '{q}':
+        client_socket.send(bytes(msg, "utf-8"))
+    else:
+        client_socket.send(bytes(msg, "utf-8"))
+        client_socket.close()
+        exit()
 
-shudown = True
-rT.join()
-s.close()
+
+if __name__ == "__main__":
+    receive_thread = Thread(target=receive)
+    receive_thread.start()
+    while True:
+        send()
--- server.py
+++ server.py
@@ -1,35 +1,52 @@
-import socket
-import time
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 
-clients = []
+clients = {}
+addresses = {}
 
-s = socket.socket()
-# host = socket.gethostname()
 host = '127.0.0.1'
 port = 5000
-s.bind((host,port))
-s.listen(5)
+bufsize = 1024
+addr = (host, port)
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(addr)
 
-quitting = False
-print("Server Started.")
-print(host)
-print(port)
-while not quitting:
-    try:
-        data, addr = s.accept()
-        # print(data.recv(1024).decode('utf-8')) funcionando
-        # print(addr)
-        # addr = s.accept()
-        # data = addr.recv(1024).decode('utf-8')
-        if "Quit" in str(data):
-            quitting = True
-        if addr not in clients:
-            clients.append(addr)
-            
-        print(time.ctime(time.time()) + str(addr) + ": :" + str(data.recv(1024).decode('utf-8')))
-        message = data.recv(1024).decode('utf-8')
-        for client in clients:
-            s.sendto(message, client)
-    except Exception as e:
-        print(e)        
-s.close()
+
+def accept_incoming_connections():
+    while True:
+        client, client_address = server.accept()
+        print("%s:%s has connected," % client_address)
+        client.send(bytes("welcome, type your name and press enter!", "utf-8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):
+    name = client.recv(bufsize).decode("utf-8")
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf-8"))
+    clients[client] = name
+    while True:
+        msg = client.recv(bufsize)
+        if msg != bytes("{q}", "utf-8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{q}", "utf-8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf-8"))
+            break
+
+
+def broadcast(msg, prefix=""):
+    for sock in clients:
+        sock.send(bytes(prefix, "utf-8")+msg)
+
+
+if __name__ == "__main__":
+    server.listen(5)
+    print("Waiting for connection...")
+    accept_thread = Thread(target=accept_incoming_connections)
+    accept_thread.start()
+    accept_thread.join()
+    server.close()
